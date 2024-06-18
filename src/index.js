15875f7c4f7d875aefaed67abf33666452cefe58
Rytsensetech <sidharthbaby77@gmail.com>
2024-06-18 11:16:17 +0530

    new4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import swDev from "./swDev"
+import * as serviceWorker from './serviceWorker';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -13,3 +15,4 @@ root.render(
 );
 
 swDev()
+serviceWorker.register();
