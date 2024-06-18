8f597972ec8f8bbee845c21d25a64c6d335dba90
Rytsensetech <sidharthbaby77@gmail.com>
2024-06-18 11:20:55 +0530

    new4

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 let cacheData = "appV1";
 this.addEventListener("install", (event) => {
-  event.waitUnitll(
+  event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
         "/static/js/main.chunk.js",
